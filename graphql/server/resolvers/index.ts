b53d51c151f51b63a36074c15d4587bdbd996324
p_irisawa <01029262@waku-2.com>
2022-10-06 22:53:52 +0900

    install codegen

--- graphql/server/resolvers/index.ts
+++ graphql/server/resolvers/index.ts
@@ -1,4 +1,6 @@
-export const resolvers = {
+import { Resolvers } from "../../__generated__/codegen/graphql";
+
+export const resolvers: Resolvers = {
   Query: {
     hello: () => "Hello from Yoga!",
   },
